fd5e52d825ad62491b9e2b2ffcdb9316e4e3d09a
cwanylisek <rewiks@gmail.com>
2020-06-24 23:41:36 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { formData } from './forms';
+
+const form = document.querySelector('form')!;
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = formData(form);
+    console.log(data);
+});
